8b9db713a4d5b1fa6be64ce35fe16c61f63fb239
Patience Umoru <umoruadahpat@gmail.com>
2021-01-17 21:57:06 +0000

    update the delete room id

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const { createRoom } = require('./shared/createRoom');
 const {listRoom } = require('./shared/listRoom');
 const { sendMessage } = require('./shared/sendMessage');
 const { sendMessageWithCards } = require('./shared/sendMessageWithCards');
-//Creates room
-//createRoom();
-//lists rooms
+
+createRoom();
 //listRoom();
 //sendMessage();
-sendMessageWithCards();
+//sendMessageWithCards();
--- shared/deleteRoom.js
+++ shared/deleteRoom.js
@@ -5,8 +5,9 @@ const webex = require('webex/env');
 const {checkStatus} = require('./checkStatus');
 const deleteRoom = async function() {
 const accessToken = process.env.WEBEX_ACCESS_TOKEN;
+
 //Replace the roomId value with the room Id of the room you want to delete
-const roomId = "Y2lzY29zcGFyazovL3VybjpURUFNOmV1LWNlbnRyYWwtMV9rL1JPT00vN2JkZjkyYTAtNTgzNi0xMWViLWEwMGUtMDVmMmM3NTlmYmQ0";
+const roomId = process.env.WEBEX_ROOM_ID;
     try{
 const fetchResponse =   await fetch(`${process.env.WEBEX_URL}/rooms/${roomId}`,
     {
